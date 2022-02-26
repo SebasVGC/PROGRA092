bcb8934b3253e3f10f9c5324f7e966f2965e25a1
SebasVGC <2902340180301@ingenieria.usac.edu.gt>
2022-02-25 18:29:05 -0600

    Clase 6 v1

---
+++ Clase 06/Clase 6/Clase 6/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.btnMenos = New System.Windows.Forms.Button()
+        Me.btnMas = New System.Windows.Forms.Button()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.lbNumero = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lbImpresion = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(800, 450)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.lbImpresion)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.lbNumero)
+        Me.TabPage1.Controls.Add(Me.btnOk)
+        Me.TabPage1.Controls.Add(Me.btnMas)
+        Me.TabPage1.Controls.Add(Me.btnMenos)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(792, 424)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Ejemplo"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(792, 424)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Tarea"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'btnMenos
+        '
+        Me.btnMenos.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMenos.Location = New System.Drawing.Point(27, 45)
+        Me.btnMenos.Name = "btnMenos"
+        Me.btnMenos.Size = New System.Drawing.Size(49, 46)
+        Me.btnMenos.TabIndex = 0
+        Me.btnMenos.Text = "-"
+        Me.btnMenos.UseVisualStyleBackColor = True
+        '
+        'btnMas
+        '
+        Me.btnMas.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMas.Location = New System.Drawing.Point(155, 45)
+        Me.btnMas.Name = "btnMas"
+        Me.btnMas.Size = New System.Drawing.Size(49, 46)
+        Me.btnMas.TabIndex = 1
+        Me.btnMas.Text = "+"
+        Me.btnMas.UseVisualStyleBackColor = True
+        '
+        'btnOk
+        '
+        Me.btnOk.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOk.Location = New System.Drawing.Point(71, 123)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(99, 41)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'lbNumero
+        '
+        Me.lbNumero.AutoSize = True
+        Me.lbNumero.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbNumero.Location = New System.Drawing.Point(100, 53)
+        Me.lbNumero.Name = "lbNumero"
+        Me.lbNumero.Size = New System.Drawing.Size(30, 31)
+        Me.lbNumero.TabIndex = 3
+        Me.lbNumero.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(282, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(131, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "1. Saludar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(282, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(235, 29)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "2. Cambiar nombre"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(282, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(233, 29)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "3. Imprimir nombre"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(282, 166)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(128, 29)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "4. Limpiar"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(282, 213)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 29)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "5. Salir"
+        '
+        'lbImpresion
+        '
+        Me.lbImpresion.AutoSize = True
+        Me.lbImpresion.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbImpresion.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.lbImpresion.Location = New System.Drawing.Point(417, 314)
+        Me.lbImpresion.Name = "lbImpresion"
+        Me.lbImpresion.Size = New System.Drawing.Size(234, 33)
+        Me.lbImpresion.TabIndex = 9
+        Me.lbImpresion.Text = "Su nombre aqui"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Clase 6"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents lbImpresion As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lbNumero As Label
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnMas As Button
+    Friend WithEvents btnMenos As Button
+    Friend WithEvents TabPage2 As TabPage
+End Class
---
+++ Clase 06/Clase 6/Clase 6/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Dim nombrePersona As String = ""
+    Private Sub btnMenos_Click(sender As Object, e As EventArgs) Handles btnMenos.Click
+        Dim numeroEntrante As String = lbNumero.Text
+        numeroEntrante = Restar(numeroEntrante)
+        lbNumero.Text = numeroEntrante
+    End Sub
+
+    Function Restar(texto As String) As String
+        Dim numero As Integer = Int32.Parse(texto)
+        Dim nuevoTexto As String
+        numero = numero - 1
+        nuevoTexto = numero.ToString()
+        Return nuevoTexto
+    End Function
+
+    Function Aumentar(texto As String) As String
+        Dim numero As Integer = Int32.Parse(texto)
+        Dim nuevoTexto As String
+        numero = numero + 1
+        nuevoTexto = numero.ToString()
+        Return nuevoTexto
+    End Function
+
+    Private Sub btnMas_Click(sender As Object, e As EventArgs) Handles btnMas.Click
+        Dim numeroEntrante As String = lbNumero.Text
+        numeroEntrante = Aumentar(numeroEntrante)
+        lbNumero.Text = numeroEntrante
+    End Sub
+
+
+    Sub opcion1()
+        'MsgBox("op 1")
+        If (nombrePersona = "") Then
+            MsgBox("Nombre vacio")
+        Else
+            MsgBox("Hola " + nombrePersona)
+        End If
+
+    End Sub
+    Sub opcion2()
+        'MsgBox("op 2")
+        Dim nuevoNombre = InputBox("Nombre", "Ingrese nombre")
+        nombrePersona = nuevoNombre
+        MsgBox("Nombre actualizado")
+    End Sub
+    Sub opcion3()
+        'MsgBox("op 3")
+        If (nombrePersona = "") Then
+            MsgBox("Nombre vacio")
+        Else
+            lbImpresion.Text = "Su nombre es " + nombrePersona
+        End If
+
+    End Sub
+    Sub opcion4()
+        'MsgBox("op 4")
+        nombrePersona = ""
+        lbImpresion.Text = "Su nombre aqui"
+        lbNumero.Text = "0"
+    End Sub
+    Sub opcion5()
+        'MsgBox("op 5")
+        Me.Close()
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Dim opcionTexto As String = lbNumero.Text
+        Dim opcionNumero As Integer = Int32.Parse(opcionTexto)
+
+        Select Case opcionNumero
+            Case 1
+                opcion1()
+            Case 2
+                opcion2()
+            Case 3
+                opcion3()
+            Case 4
+                opcion4()
+            Case 5
+                opcion5()
+            Case Else
+                MsgBox("Opcion incorrecta")
+
+        End Select
+    End Sub
+End Class
