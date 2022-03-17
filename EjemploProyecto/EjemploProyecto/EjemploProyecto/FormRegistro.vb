9d647153e7aa873a075f7b90e455088fe6db2e48
SebasVGC <2902340180301@ingenieria.usac.edu.gt>
2022-03-17 15:17:29 -0600

    corrección

--- EjemploProyecto/EjemploProyecto/EjemploProyecto/FormRegistro.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FormRegistro
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Form1.Visible = True
-        Me.Visible = False
-    End Sub
-End Class
